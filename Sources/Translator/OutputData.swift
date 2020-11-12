9aa12f45933a8e354351f39abe11ac25821df2ee
SmolinaNadya <nadusha8032000@gmail.com>
2020-11-12 23:35:02 +0700

    Новый класс и протокол для корректного вывода в строку

---
+++ Sources/Translator/OutputData.swift
@@ -0,0 +1,39 @@
+class OutputData: OutputDataProtocol {
+    private var lines = ""
+    func outputLanguageAndValue(dictionary: [String:String]) -> String {
+        for (language, value) in dictionary {
+            if lines == "" {
+                lines = "\(language):\(value)"
+            }
+            else {
+                lines += "\n\(language):\(value)"
+            }     
+        }
+        return lines
+    }
+    func outputKeyAndValue (dictionary: [String:String]) -> String {
+        for (key, value) in dictionary {
+            if lines == "" {
+                lines = "\(key) = \(value)"
+            }
+            else {
+                lines += "\n\(key) = \(value)"
+            }
+        }
+        return lines
+    }
+    func outputAllData (dictionary: [String: [String: String]]) -> String {
+        for (word, dictionary) in words {
+            if lines == "" {
+                lines = word
+            }
+            else {
+                lines += "\n\(word)"
+            }
+            for (language, value) in dictionary {
+                    lines += "\n\(language):\(value)"
+                }
+        }
+        return lines
+    }
+}
