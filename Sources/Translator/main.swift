b350fb049ea90e604d34f6f007138d170468a9f1
SmolinaNadya <nadusha8032000@gmail.com>
2020-11-12 23:39:09 +0700

    Добавила отступы у case и протокол outputData

--- Sources/Translator/main.swift
+++ Sources/Translator/main.swift
@@ -1,23 +1,26 @@
 import Foundation
 import ArgumentParser
+let container = Container()
+var words = container.getData.getData()
 func main() {
     let container = Container()
     let parser = container.argumentParser
     guard let arguments = parser.toParse() else {
-        container.printer.printingData(Data: container.help.help())
+        container.printer.printingData(data: container.help.help())
         return
     }
     switch arguments {
         case .search(let key, let language):
-        let lines = container.search.search(key: key, language: language)
-        container.printer.printingData(Data: lines)
+          let lines = container.search.search(key: key, language: language)
+          container.printer.printingData(data: lines)
         case .update(let word, let key, let language):
-        container.update.update(newWord: word, key: key, language: language)
+          container.update.update(newWord: word, key: key, language: language)
         case .delete(let key, let language):
-        container.delete.delete(key: key, language: language)
+          container.delete.delete(key: key, language: language)
     }  
 }
 main()
+
 class Container {
     var argumentParser: ArgumentParserProtocol {
         return ArgumentParser()
@@ -28,8 +31,11 @@ class Container {
     var getData: GetDataProtocol {
         return GetData()
     }
+     var outputData: OutputDataProtocol {
+        return OutputData()
+    }
     var search: SearchProtocol {
-        return Search(getterData: getData)
+        return Search(getterData: getData, outputData: outputData)
     }
     var update: UpdateProtocol {
         return Update(getterData: getData, writer: writer)
